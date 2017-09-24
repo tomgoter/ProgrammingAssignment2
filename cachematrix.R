5388b1672465ee183d55eac1def349979dd16e84
tomgoter <tomgoter@gmail.com>
2017-09-24 17:50:47 -0400

    R Programming Assignment - Cache Inverse Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script contains two functions which will be used to 
+## cache the inverse of a matrix.  This is part of the R Programming
+## Coursera coursework.
 
-## Write a short comment describing this function
+## This function is used to create a special matrix object that can 
+## cache its inverse.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## i is the inverse of the matrix and is initialized to null
+    i <- NULL
+    ## setMatrix is a function that takes an arugment, y (matrix)
+    ## and passes it into the x variable
+    setMatrix <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    ## getMatrix will simply return the matrix, x, set with setMatrix
+    getMatrix <- function () x
+    ## setInverse will take as input an inverse matrix
+    ## and pass it into the i variable
+    setInverse <- function (inverseMatrix) i <<- inverseMatrix
+    ## getInverse will simply return i, the inverse matrix
+    getInverse <- function() i
+    list(setMatrix = setMatrix,
+         getMatrix = getMatrix,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function actually computes the inverse of the matrix
+## if it is not in cache already and it has not been modified
+## since being cached.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    ## Check if the inverse was actually in cache. 
+    ## If so, end early and return the value
+    if(!is.null(i)) {
+        message("Getting the inverted matrix from cache!")
+        return(i)
+    }
+    matrixIn <- x$getMatrix()
+    i <- solve(matrixIn, ...)
+    x$setInverse(i)
+    i
+    }
